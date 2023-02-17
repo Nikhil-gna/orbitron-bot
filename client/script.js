a6474085130e3b4739e5a7762881dee26044d32b
Nikhil Anand <102014379+Nikhil-gna@users.noreply.github.com>
2023-02-17 20:50:23 +0530

    url and img update

--- client/script.js
+++ client/script.js
@@ -83,7 +83,7 @@ const handleSubmit = async(e) => {
 
     //fetch data from server --> bot rresponse
 
-    const response = await fetch('http://localhost:5000/', {
+    const response = await fetch('https://orbitron.onrender.com', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
